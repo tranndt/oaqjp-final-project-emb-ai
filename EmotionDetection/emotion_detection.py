5ec1f32b4aca3d9946128ca5ce77fc2913e75e21
tranndt <duythongvt1@gmail.com>
2024-07-18 11:42:14 -0400

    “âTask 1-2

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    data = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url,json=data,headers=headers)
+    fmt_response = json.loads(response.text)
+
+    return fmt_response
+    
